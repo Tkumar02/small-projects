ece766d212139838b261682b9943301e0b565b61
Tkumar02 <ta_265@yahoo.co.uk>
2024-02-21 23:07:37 +0000

    Completed loadmore See comment for double render

--- src/components/loadMore/index.jsx
+++ src/components/loadMore/index.jsx
@@ -1,23 +1,28 @@
 import { useEffect, useState } from "react";
+import './styles.css'
+
+//STRICT MODE IN INDEX.JS CAUSES DOUBLE RENDERING THEREBY CAUSING 20+20 TOTAL 40 PRODUCTS TO LOAD ON FIRST LOAD OF WEBPAGE, REST SEEMS TO BE WORKING OK.
 
 export default function LoadMoreData(){
 
     const[loading,setLoading]=useState(false)
     const[products,setProducts] = useState([])
     const[count,setCount] = useState(0)
-    const[nextProducts,setNextProducts] = useState([])
-    const[countProducts,setCountProducts] = useState(0)
+    // const[nextProducts,setNextProducts] = useState([])
+    // const[countProducts,setCountProducts] = useState(0)
+    const[disableButton,setDisableButton] = useState(false)
 
     async function fetchProducts(){
         try{
             setLoading(true)
-            const response = await fetch (`https://dummyjson.com/products?limit=10&skip=${count===0 ? 0 : count*10}`);
+            const response = await fetch (`https://dummyjson.com/products?limit=20&skip=${count===0 ? 0 : count*20}`);
             const result = await response.json()
-            console.log(result, 'length=',result.products.length)
 
             if(result&& result.products && result.products.length){
-                setProducts(result.products)
+                // setProducts(result.products)
+                setProducts((prevData)=>[...prevData, ...result.products])
                 setLoading(false)
+                console.log(result, 'length=',result.products.length)
             }
 
         } catch(e) {
@@ -26,20 +31,26 @@ export default function LoadMoreData(){
         }
     }
 
-    async function loadMore(){
-        const response = await fetch(`https://dummyjson.com/products?limit=10&skip=${count+10}`)
-        const result = await response.json()
-        let nextSet = [...nextProducts]
-        nextSet.push(result.products)
-        setNextProducts(nextSet)
-        console.log(nextProducts,'length of nextProducts = ', nextProducts.length)
-        setCountProducts(countProducts+1)
-        setCount(count+10)
-    }
+    // async function loadMore(){
+    //     const response = await fetch(`https://dummyjson.com/products?limit=10&skip=${count+10}`)
+    //     const result = await response.json()
+    //     let nextSet = [...nextProducts]
+    //     nextSet.push(result.products)
+    //     setNextProducts(nextSet)
+    //     console.log(nextProducts,'length of nextProducts = ', nextProducts.length)
+    //     setCountProducts(countProducts+1)
+    //     setCount(count+10)
+    // }
 
     useEffect(()=>{
         fetchProducts()
-    },[])
+    },[count])
+
+    useEffect(()=>{
+        if(products && products.length===100){
+            setDisableButton(true)
+        }
+    },[products])
 
     if(loading){
         return (<div>Loading data, please wait...</div>)
@@ -47,20 +58,29 @@ export default function LoadMoreData(){
 
     return(<>
         <div className="container">
-        {products && products.length ? (
-        products.map(product=>
-        <div key={product.id}>
-            <img src={product.thumbnail} alt={product.title} />
-        </div>)
-        ) : null}
+        <div className="product-container">
+            {products && products.length ? (
+            products.map(product=>
+            <div className="product" key={product.id}>
+                <img src={product.thumbnail} alt={product.title} />
+                <p>{product.title}</p>
+            </div>)
+            ) : null}
+        
 
-        {nextProducts ? nextProducts.map(innerArray =>
+        {/* {nextProducts ? nextProducts.map(innerArray =>
             innerArray.map((product) =>
-                <div key={product.id}>
+                <div className="product" key={product.id}>
                     <img src={product.thumbnail} alt={product.title}/>
-                </div>)) : null}
+                    <p>{product.title}</p>
+                </div>)) : null} */}
+        
+        </div>
             
-        <button onClick={loadMore}>Load more data</button>
+        <div className="button-container">
+            {/* <button onClick={loadMore}>Load more data</button> */}
+            <button disabled={disableButton} onClick={()=>setCount(count+1)}>Load more data</button>
+        </div>
         </div>
     </>)
 }
